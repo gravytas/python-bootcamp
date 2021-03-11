699b75831b72e3a26fc42905884ca9bd2f819dba
gravytas <80134510+gravytas@users.noreply.github.com>
2021-03-10 20:11:49 -0500

    Add files via upload

---
+++ Python Bootcamp Day 6_Python  Functions/reeborg maze.py
@@ -0,0 +1,22 @@
+#Reeborg Maze
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif wall_on_right() and front_is_clear():
+        move()
+    elif wall_in_front():
+        turn_left()
+            
+    
+    
+        
+    
+    
+    
